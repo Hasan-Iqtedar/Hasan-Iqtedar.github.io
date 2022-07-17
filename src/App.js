e59c32c0b033a61fbde7160c819575cd638a9720
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-17 13:14:09 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+        <div className="container">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Skill</a>
+          <a href="">Contact</a>
+        </div>
+      </Header>
+
+      {/* <Home />
+      <About />
+      <Skills />
+      <Contact /> */}
+      
+    </div>
+  );
+}
+
+export default App;
