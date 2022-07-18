2f8bb6b90fff3c9c0dff747f7a42c44ddf543d4b
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-18 05:53:36 +0500

    Add about section

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import "../styles/about.css";
+
+const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <h1>About Me</h1>
+        <div className="content">
+          Being a Computer Science major, I possess a wide spectrum of technical
+          knowledge covering various branches of Computer Science. My areas of
+          expertise lie in problem solving and algorithmic analysis crucial for
+          producing efficient software and scalable solutions.
+          <br></br>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
