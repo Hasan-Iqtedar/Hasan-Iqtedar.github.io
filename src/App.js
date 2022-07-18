8e6bd849dbadb23efae5171217f55133841354d5
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-18 16:35:04 +0500

    Add sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
-
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-
       <About />
-      {/* <Skills />
-      <Contact /> */}
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
