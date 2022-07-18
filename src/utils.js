076333f4aaf17f1b0fb004744ee474b0e6e2286e
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-18 15:43:11 +0500

    Add utility to import images in object form

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,18 @@
-export function importImages(regex) {
+export function importImagesArray(regex) {
   let images = [];
   regex.keys().forEach((item) => {
     images.push({
       data: regex(item),
       name: item.replace("./", "").split(".")[0],
     });
+  });
+  return images;
+}
+
+export function importImagesObject(regex) {
+  let images = {};
+  regex.keys().forEach((item) => {
+    images[item.replace("./", "").split(".")[0]] = regex(item);
   });
   return images;
 }
